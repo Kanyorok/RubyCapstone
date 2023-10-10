b8efd252c76f092414d1d09cad6006c018abd61d
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-10-10 10:04:03 +0300

    Create musicalbum and genre class with relevant properties

--- lib/music/music.rb
+++ lib/music/music.rb
@@ -1,4 +1,16 @@
 require_relative '../catalogue/catalogue'
+require 'date'
 
 class Music < Catalogue
+  attr_reader :id, :on_spotify
+
+  def initialize(publish_date, on_spotify)
+    super(publish_date)
+    @id = Random.rand(1...100)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    (Date.today.year - @publish_date.year) >= 10 && @on_spotify == true
+  end
 end
