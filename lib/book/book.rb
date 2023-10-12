927fd7b69e644e99b1037fcaa4e74ce8665c9120
mathsoft29 <iwuconcept@gmail.com>
2023-10-12 15:32:34 +0100

    Adjust the book.rb class

--- lib/book/book.rb
+++ lib/book/book.rb
@@ -1,11 +1,10 @@
 require_relative '../catalogue/catalogue'
 
 class Book < Catalogue
-  attr_reader :id
   attr_accessor :publisher, :cover_state
-  def initialize(publisher,cover_state,publish_date, archived)
+
+  def initialize(publisher, cover_state, publish_date, archived)
     super(publish_date, archived)
-    @id = Random.rand(1...100)
     @publisher = publisher
     @cover_state = cover_state
   end
@@ -15,13 +14,4 @@ class Book < Catalogue
 
     false
   end
-end
-
-def to_hash
-    item_hash = super
-    book_hash = {
-     'publisher' => @publisher,
-     'cover_state' => @cover_state
-    }
-item_hash.merge(book_hash)
 end
