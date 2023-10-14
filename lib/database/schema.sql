730212ca5370090205fc7af73ade0f5005746ecf
mathsoft29 <iwuconcept@gmail.com>
2023-10-14 10:26:34 +0100

    Add the schema.sql

--- lib/database/schema.sql
+++ lib/database/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE music_albums (
 	release_date DATE,
 	album_title VARCHAR(255),
 	album_artist VARCHAR(255),
-	on_spotify BOOL
+	on_spotify BOOL,
   genre_id INT
   ADD CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genre(id)
 );
